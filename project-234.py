9bfa4af2970f073b278ecdf12d4931a3075a2404
arjunTheCoder <82092827+arjun380@users.noreply.github.com>
2023-02-16 15:35:50 +0530

    Add files via upload

---
+++ project-234.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+dataset = pd.read_csv('projectC234_EPL.csv')
+goal_column=dataset['Goals']
+sorted_club_data_by_ascending_order = goal_column.groupby(dataset['Club'])
+
+sum_of_goal_by_each_club=sorted_club_data_by_ascending_order.sum()
+sorted_goal_values = sum_of_goal_by_each_club.sort_values(ascending=True)
+print("goals of each club:-",sorted_goal_values)
+epl_top_goals=dataset.sort_values(by=['Goal'],ascending=False)
+fig=px.bar(epl_top_goals,x='name',y='goals',color='green',hover_data=['club','age'],text='goals')
+fig.show()
+
